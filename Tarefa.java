0fc5cf377c28c2bf88bb12e3372081d815bb8339
João <joaosantanaguilherme@gmail.com>
2026-05-12 21:24:07 -0300

    feat: adição de prazo

--- Tarefa.java
+++ Tarefa.java
@@ -2,11 +2,13 @@ public class Tarefa {
     String descricao;
     boolean concluido;
     String prioridade;
+    String data_entrega;
     
     public Tarefa(String descricao) {
         this.descricao = descricao;
         this.concluido = false;
         this.prioridade = prioridade;
+        this.data_entrega = data_entrega;
     }
 
 
